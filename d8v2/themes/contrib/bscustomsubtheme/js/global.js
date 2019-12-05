784969bd2f6ec80c3d76ec13ab4c7d8f91203e34
Nadia Hussain <nadia@mantaraymedia.com>
2019-12-05 13:59:49 +0000

    + d8paronly

---
+++ d8v2/themes/contrib/bscustomsubtheme/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.bscustomsubtheme = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
